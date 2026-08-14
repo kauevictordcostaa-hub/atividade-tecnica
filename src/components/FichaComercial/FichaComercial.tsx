3b237d6840877b3276b09416be1d36889f699308
kauevictordcostaa-hub <kauevictordcostaa@gmail.com>
2026-08-14 14:53:26 -0300

    implementacao formulario

--- src/components/FichaComercial/FichaComercial.tsx
+++ src/components/FichaComercial/FichaComercial.tsx
@@ -1,18 +1,33 @@
-"use client";
-
 import styles from "./FichaComercial.module.css";
 
 export default function FichaComercial() {
   return (
     <main className={styles.container}>
-      <section className={styles.card}>
-        <h1>Ficha de Cadastro de Imóvel Comercial</h1>
+      <div className={styles.card}>
+        <h1>Cadastro de Imóvel Comercial</h1>
 
-        <p>
-          Preencha os dados abaixo para realizar o cadastro para locação do
-          imóvel comercial.
-        </p>
-      </section>
+        <form>
+          <label>Razão Social</label>
+          <input type="text" required />
+
+          <label>Nome Fantasia</label>
+          <input type="text" required />
+
+          <label>CNPJ</label>
+          <input type="text" required />
+
+          <label>Inscrição Estadual</label>
+          <input type="text" required />
+
+          <label>E-mail</label>
+          <input type="email" required />
+
+          <label>Nome do Responsável</label>
+          <input type="text" required />
+
+          <button type="submit">Enviar dados</button>
+        </form>
+      </div>
     </main>
   );
 }

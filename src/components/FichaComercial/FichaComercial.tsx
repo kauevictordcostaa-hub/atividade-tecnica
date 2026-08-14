f598560cd69cc978fe2a6b4ec004f4789f1d088b
kauevictordcostaa-hub <kauevictordcostaa@gmail.com>
2026-08-14 14:54:19 -0300

    adicao de placeholders aos campos

--- src/components/FichaComercial/FichaComercial.tsx
+++ src/components/FichaComercial/FichaComercial.tsx
@@ -8,22 +8,46 @@ export default function FichaComercial() {
 
         <form>
           <label>Razão Social</label>
-          <input type="text" required />
+          <input
+            type="text"
+            placeholder="Digite a razão social"
+            required
+          />
 
           <label>Nome Fantasia</label>
-          <input type="text" required />
+          <input
+            type="text"
+            placeholder="Digite o nome fantasia"
+            required
+          />
 
           <label>CNPJ</label>
-          <input type="text" required />
+          <input
+            type="text"
+            placeholder="00.000.000/0000-00"
+            required
+          />
 
           <label>Inscrição Estadual</label>
-          <input type="text" required />
+          <input
+            type="text"
+            placeholder="Digite a inscrição estadual"
+            required
+          />
 
           <label>E-mail</label>
-          <input type="email" required />
+          <input
+            type="email"
+            placeholder="empresa@email.com"
+            required
+          />
 
           <label>Nome do Responsável</label>
-          <input type="text" required />
+          <input
+            type="text"
+            placeholder="Digite o nome do responsável"
+            required
+          />
 
           <button type="submit">Enviar dados</button>
         </form>
